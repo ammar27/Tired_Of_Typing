03f021c001e99855287b043383b06a77de0d7b30
Ammar Bagasrawala <ammarb27@gmail.com>
2016-09-01 14:08:44 +1200

    Added typings for typescript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello");
+var start_btn = $("#start")[0];
+var subscription = "b9ae29e86b0d42a7b7c2185cd566d57d";
+function start() {
+}
+function stop() {
+}
--- js/main.ts
+++ js/main.ts
@@ -1,2 +1,10 @@
+var start_btn = $("#start")[0];
+var subscription: string = "b9ae29e86b0d42a7b7c2185cd566d57d";
 
-console.log("Hello");
+function start(): void {
+
+}
+
+function stop(): void {
+
+}
